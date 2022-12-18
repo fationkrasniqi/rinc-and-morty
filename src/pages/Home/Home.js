08136be9ecdef11ef1ad8e04af7da9fee7ca106c
Fation Krasniqi <fationkrasniqi777@gmail.com>
2022-12-18 20:54:30 +0100

    Trailer added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -108,39 +108,51 @@ const Home = () => {
           </figurecaption>
 
           </figure>
-        
-         
           
-          {/* <div className='castcard'>
-            <img  className='castimages' src='http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQqu6sbizVCs_rOvjY61SY26RUv5dcVBZ6fHMSRK0WHm5KleQM_hDrZhTHwkQlJq0dr_-d6FlSHUfUN6DY'></img>
-            <h1>Chris Parnell</h1>
-            <p>Jerry Smith</p>
-            <p>61 episodes ` 2013-2022</p>
+          <figure>
+          <div className='hover-animation'>
+           
+              <img  className="img-back" src='https://www.kindpng.com/picc/m/43-438936_beth-smith-rick-and-morty-wine-hd-png.png'></img>
+              <img className='img-front' src='http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQqog_xpIkjXRWxL9rNE2fLjGFm2FZHoOtZfzyBmOxlDDDs1DPVlVEWxRMldjXweMQrk-K4UwSky4DJi-w'></img>
           </div>
-          <div className='castcard'>
-            <img className='castimages'  src='http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQo5eX9vFQJwS2jkIrLU3f-p5xAvRvllVfVggYHbOAwwO4RX0ZpGZYeHr6rnrmbALkmDRjJiIq8ON7twgU'></img>
-            <h1>Spencer Grammer</h1>
-            <p>Summer Smith</p>
+          <figurecaption>
+          <h1>Sarah Chalke</h1>
+            <p>Beth Smith</p>
             <p>61 episodes ` 2013-2022</p>
+          </figurecaption>
+
+          </figure>
+
+          <figure>
+          <div className='hover-animation'>
+           
+              <img  className="img-back" src='https://i.pinimg.com/474x/d9/15/8c/d9158ce24f1901a59be5db706730b55d.jpg'></img>
+              <img className='img-front' src='http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcTogD52zFSVexEGDdLmVvRr0Sd98PG8wnms5G4kJOZs91M1A-S4d_6S0DZudgCRLLtzU59GbMJmN3bIA_M'></img>
           </div>
-          <div className='castcard'>
-            <img  className='castimages' src='http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQqog_xpIkjXRWxL9rNE2fLjGFm2FZHoOtZfzyBmOxlDDDs1DPVlVEWxRMldjXweMQrk-K4UwSky4DJi-w'></img>
-            <h1>Sarah Chalke</h1>
-            <p>Summer Smith</p>
-            <p>61 episodes ` 2013-2022</p>
-          </div>
-          <div className='castcard'>
-            <img className='castimages' src='http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcTogD52zFSVexEGDdLmVvRr0Sd98PG8wnms5G4kJOZs91M1A-S4d_6S0DZudgCRLLtzU59GbMJmN3bIA_M'></img>
-            <h1>Kari Wahlgren</h1>
+          <figurecaption>
+          <h1>Kari Wahlgren</h1>
             <p>Jessica</p>
             <p>36 episodes ` 2013-2022</p>
+          </figurecaption>
+
+          </figure>
+         
+          <figure>
+          <div className='hover-animation'>
+           
+              <img  className="img-back" src='https://i1.sndcdn.com/avatars-000530476173-1wgpia-t500x500.jpg'></img>
+              <img className='img-front' src='http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcTY6VmMa4PAjDETyXnlRsPvQ6Kdv3-9tmhdEVy1fJkZYxol1rMbV7uRroWXIUPT-iUzLpxkSgSCMK6v_uA'></img>
           </div>
-          <div className='castcard'>
-            <img className='castimages' src='http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcTY6VmMa4PAjDETyXnlRsPvQ6Kdv3-9tmhdEVy1fJkZYxol1rMbV7uRroWXIUPT-iUzLpxkSgSCMK6v_uA'></img>
-            <h1>Maurice LaMarche</h1>
+          <figurecaption>
+          <h1>Maurice LaMarche</h1>
             <p>Abradoph Lindler</p>
             <p>29 episodes ` 2013-2022</p>
-          </div> */}
+          </figurecaption>
+
+          </figure>
+        
+        {/* End of Top cast */}
+
         </div>
         <div>
 
@@ -150,9 +162,36 @@ const Home = () => {
             onClick={()=> navigate('/about')}
             >View more info </button>
         </div>
-       
+        <h1 className='youtubetexting'>    First teaser trailer for Rick and morty live action starring Christopher Lloyd, Jaeden Martel.</h1>
+
+       <div className='containervideosrc'>
+        <iframe className='' width="420" height="315" src='https://www.youtube.com/embed/b57Weqj13e0' 
+        frameborder="0"
+       allowfullscreen
+       ng-show="showvideo"
+        title='Rick and morty'
+        >
+        
+        </iframe>
+        <h4 className='youtubetext'>
+    
+        After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth's
+         doorstep to move in with her and her family. Although Beth welcomes Rick into her home, her husband,
+          Jerry, isn't as happy about the family reunion. Jerry is concerned about Rick, a sociopathic scientist,
+           using the garage as his personal laboratory. In the lab, Rick works on a number of sci-fi gadgets,
+            some of which could be considered dangerous. But that's not all Rick does that concerns Jerry. 
+            He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.
+        </h4>
+       </div>
         <div className='test2'>
-            <h1>Make a component  "footer"</h1>
+        <div class="card">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
         </div>
     </div>
     
